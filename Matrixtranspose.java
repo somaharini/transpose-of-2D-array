d0463da8546c5d6c58b20f8a509a4fbc1f057461
somaharini <somaharini24@gmail.com>
2025-01-30 22:28:25 +0530

    Create Matrixtranspose.java

---
+++ Matrixtranspose.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Matrixtranspose {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int cols = scanner.nextInt();
+        int[][] matrix = new int[rows][cols];
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        int[][] transpose = new int[cols][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        System.out.println("Transposed matrix:");
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+              System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
+
